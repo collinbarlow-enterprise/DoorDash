fa7251951fa0418b86ffab39d3e819f18bec819f
CollinBarlow <collinbarlow@gmail.com>
2023-11-15 09:47:51 -0800

    first commit

---
+++ src/pages/App/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import './App.css'
+import NewOrderPage from '../NewOrderPage/NewOrderPage'
+import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage'
+import AuthPage from '../AuthPage/AuthPage'
+import { Routes, Route } from 'react-router-dom';
+import NavBar from '../../components/NavBar/NavBar'
+import { getUser } from '../../utilities/users-service';
+
+export default function App() {
+
+  const [user, setUser] = useState(getUser())
+
+  function updateUser(userState){
+    setUser(userState)
+  }
+
+  return (
+    <main className="App">
+
+      {user ?
+        <>
+          <NavBar user={user} updateUser={updateUser}/>
+          <Routes>
+            <Route path="/orders/" element={<OrderHistoryPage />} />
+            <Route path="/orders/new" element={<NewOrderPage />} />
+          </Routes>
+        </>
+        :
+        <AuthPage user={user} setUser={updateUser} />
+      }
+    </main>
+  )
+}
+
