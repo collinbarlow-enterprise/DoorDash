aff0afa13a9dd01d1040681238416ec82b0f0d3e
CollinBarlow <collinbarlow@gmail.com>
2023-11-17 14:19:49 -0800

    able to iterate over restaurant.menu now to populate the restaurants dishes

--- src/components/RestaurantPageComponent/RestaurantPageComponent.jsx
+++ src/components/RestaurantPageComponent/RestaurantPageComponent.jsx
@@ -12,6 +12,7 @@ export default function RestaurantPageComponent({}) {
     try {
       const restaurantData = await restaurantsAPI.getOneRestaurant(restaurantID);
       setRestaurant(restaurantData);
+      console.log(restaurant,'restaurant in restaurantPageComponent')
     } catch (error) {
       console.error(error, 'error for getRestaurant in RestaurantPageComponent')
     }
@@ -24,6 +25,18 @@ useEffect(() => {
   return (
     <div className="container">
       <h6 className="text-center">Restaurant Page Component</h6>
+      <div>{restaurant.name}</div>
+      <div>{restaurant.cuisineType}</div>
+      <div>
+      <h6>Menu:</h6>
+      <ul>
+        {restaurant.menu?.map((menuItem, index) => (
+          <li key={index}>
+            <strong>{menuItem.dishName}</strong>: {menuItem.description} - ${menuItem.price.toFixed(2)}
+          </li>
+        ))}
+      </ul>
+    </div>
     </div>
   )
 }
