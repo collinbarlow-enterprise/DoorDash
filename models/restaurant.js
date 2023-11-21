d01bc068b069ffe574bcc45550d3336ba19ce1a9
CollinBarlow <collinbarlow@gmail.com>
2023-11-21 10:27:31 -0800

    reseeded database with embedded menu items subdocuments

--- models/restaurant.js
+++ models/restaurant.js
@@ -1,26 +1,22 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-
-const restaurantSchema = new Schema({
-    name: {type: String},
-    cuisineType: {type: String},
-    picture: {type: String},
-    menu: [{
-        dishName: { type: String },
-        description: { type: String },
-        ingredients: [{ type: String }],
-        price: { type: Number },
-        ratings: { type: Number },
-        reviews: [{ type: String }],
-    }],
-    
-    // if menu items are in an array, how do i access avgRating?
-    // will need to reseed data after changing this 
-    
-    avgRating: [{type: Schema.Types.ObjectId, ref: 'menuItem'}],
-    orderHistory: {type: Schema.Types.ObjectId, ref: 'Order'}
-
+const menuItemSchema = new Schema({
+    dishName: { type: String },
+    description: { type: String },
+    ingredients: [{ type: String }],
+    price: { type: Number },
+    ratings: { type: Number },
+    reviews: [{ type: String }],
 });
 
-module.exports = mongoose.model("Restaurant", restaurantSchema)
+const restaurantSchema = new Schema({
+    name: { type: String },
+    cuisineType: { type: String },
+    picture: { type: String },
+    menu: [menuItemSchema], // Embed the menuItemSchema directly
+    avgRating: [{ type: Schema.Types.ObjectId, ref: 'menuItem' }],
+    orderHistory: { type: Schema.Types.ObjectId, ref: 'Order' },
+});
+
+module.exports = mongoose.model("Restaurant", restaurantSchema);
