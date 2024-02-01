e736b552d5c2a7edbe1698f511baade498bb926d
CollinBarlow <collinbarlow@gmail.com>
2024-02-01 10:44:39 -0800

    created new paidOrder model and tweaked the driver model

--- models/driver.js
+++ models/driver.js
@@ -4,13 +4,16 @@ const Schema = mongoose.Schema;
 // not used in this version of Magic Chef
 
 const driverSchema = new Schema({
-    name: {type: String},
-    // url string 
-    picture: {type: String},
-    // can make these have subfields for make, model, year
-    car: {type: String},
-    // would be an array that the user could push to, may need to create two properties for the rating field for avg and an array, or add another field that would be ratingAvg
-    rating: [{type: Number}]
-});
+    firstName: { type: String },
+    lastName: { type: String },
+    // url string for the image
+    picture: { type: String, default: 'not available' },
+    car: {
+      make: { type: String },
+      model: { type: String },
+      year: { type: Number }
+    },
+    rating: [{ type: Number }]
+  });
 
 module.exports = mongoose.model("Driver", driverSchema)
---
+++ models/paidOrder.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const lineItemSchema = new Schema({
+    quantity: { type: Number },
+    item: { type: Schema.Types.ObjectId, ref: 'MenuItem' },
+    price: { type: Number },
+}, {
+    timestamps: true,
+    toJSON: { virtuals: true }
+});
+
+lineItemSchema.virtual('extPrice').get(function() {
+    return this.quantity * this.price;
+});
+
+const paidOrderSchema = new Schema({
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    restaurant: { type: Schema.Types.ObjectId, ref: 'Restaurant', required: false },
+    orderItems: [{ type: Schema.Types.ObjectId, ref: 'Restaurant', required: false }],
+    driver: { type: Schema.Types.ObjectId, ref: 'Driver', required: false },
+    totalPrice: { type: Number },
+    status: { type: String },
+    feesAndTaxes: { type: Number, default: 0 },
+    promoCode: { type: Boolean, default: false },
+    promoCodeApplied: { type: String, default: 'none' },
+    promoCodeDiscount: { type: Number, default: 0 },
+    isGift: { type: Boolean, default: false },
+    deliveryOption: {
+        type: {
+            type: String,
+            enum: ['standard', 'schedule ahead'],
+            default: 'standard',
+        },
+        data: {
+            type: Date,
+            default: null,
+            validate: {
+                validator: function(date) {
+                    return date === null || date > new Date();
+                },
+                message: 'Invalid date. Date must be in the future.',
+            },
+        },
+    },
+    deliveryStatus: {type: String, enum: ['order received', 'order in progress', 'waiting on driver', 'driver on route', 'order delivered']},
+    orderDelived: {type: Boolean, default: false},
+    dropOffInstructions: { type: String, default: 'none' },
+    lineItems: [lineItemSchema],
+    isPaid: { type: Boolean, default: false },
+}, {
+    timestamps: true,
+    toJSON: { virtuals: true }
+});
+
+module.exports = mongoose.model("PaidOrder", paidOrderSchema)
