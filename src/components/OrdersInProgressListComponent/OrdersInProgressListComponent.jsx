e300b2f1aa47f41b2608cc15b4c4cbd1f4498911
CollinBarlow <collinbarlow@gmail.com>
2024-05-03 12:05:00 -0700

    finished for the week, styling and functionality updated, working on small bugs

--- src/components/OrdersInProgressListComponent/OrdersInProgressListComponent.jsx
+++ src/components/OrdersInProgressListComponent/OrdersInProgressListComponent.jsx
@@ -9,32 +9,45 @@ const OrdersInProgressListComponent = ({ orders }) => {
       <ul>
         {orders.map(order => (
           <li key={order.id}>
-          <div className='order-info'>Order Status <span>{order.deliveryStatus}</span></div>
-          <div className='order-info'>Order ID: <span>{order.id}</span></div>
-          <div className='order-info'>Order Placed: <span>{order.createdAt}</span></div>
-          <div className='order-info'>Restaurant: <span>{order.restaurant}</span></div>
-          <div className='order-info'>Order Amount: <span>{order.totalPrice}</span></div>
-          <div className='order-info'># of Items: <span>{order.lineItems.length}</span></div>
-          <ul className="line-items-list">
+            <div className='order-info'>Order Status <span>{order.deliveryStatus.toUpperCase()}</span></div>
+            <div className='order-info'>Order ID: <span>{order.id}</span></div>
+            <div className='order-info'>Order Placed:   <span>
+              {
+                new Date(order.createdAt).toLocaleString('en-US', {
+                  year: 'numeric', month: 'long', day: 'numeric',
+                  hour: '2-digit', minute: '2-digit'
+                })
+              }
+            </span></div>
+            <div className='order-info'>Restaurant: <span>{order.restaurantName}</span></div>
+            <div className='order-info'>Order Amount: <span>${order.totalPrice}</span></div>
+            <div className='order-info'># of Items: <span>{order.lineItems.length}</span></div>
+            <ul className="line-items-list">
               {order.lineItems.map(item => (
                 <li key={item._id} className="line-item">
                   <div className="left-info">
-                    <span className="item-id">Item ID: {item._id}</span>
+                    <span className="item-id">Item: {item.itemName}</span>
                     <span className="quantity">Quantity: {item.quantity}</span>
                   </div>
                   <div className="right-info">
-                    <span className="price">Price: {item.price}</span>
+                    <span className="price">Price: ${item.price}</span>
                   </div>
                 </li>
               ))}
             </ul>
             <div className='order-info'>Drop Off Instructions: <span>{order.dropOffInstructions}</span></div>
-          <div className='order-info'>Order is a Gift? <span>{order.isGift}</span></div>
+            {order.isGift ? (
+              <>
+                <div className='order-info'>Order is a Gift? <span>{order.isGift}</span></div>
+              </>
+            ) : (
+              <></>
+            )}
+            <br />
           </li>
         ))}
       </ul>
     </div>
   );
 };
-
 export default OrdersInProgressListComponent;
