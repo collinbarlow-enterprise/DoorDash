601dad422db9c9f56c349ea1112be5dbbd83aa51
CollinBarlow <collinbarlow@gmail.com>
2024-12-29 23:28:06 -0800

    added winston logger

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 //variables
+console.log('hello')
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
+const winstonLogger = require('./src/utilities/logger.js');
 const ensureLoggedIn = require('./config/ensureLoggedIn');
 
 require('dotenv').config();
@@ -11,6 +13,11 @@ const app = express();
 
 //middleware
 app.use(logger('dev'));
+
+// app.use((req, res, next) => { 
+//   winstonLogger.http(`[HTTP] ${req.method} ${req.url}`);
+//   next();
+// })
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'dist')));
 
@@ -32,6 +39,7 @@ app.get('/*', function (req, res) {
 
 //listener
 const port = process.env.PORT || 3000;
+console.log('made it to listener')
 app.listen(port, function () {
   console.log(`Express app running on port ${port}`)
 });
---
+++ src/utilities/logger.js
@@ -0,0 +1,27 @@
+// logger.js
+const { createLogger, format, transports } = require('winston');
+
+const logger = createLogger({
+  level: 'info',
+  format: format.combine(
+    format.timestamp(),
+    format.json()
+  ),
+  defaultMeta: { service: 'your-app-name' },
+  transports: [
+    new transports.File({ filename: 'error.log', level: 'error' }),
+    new transports.File({ filename: 'combined.log' })
+  ]
+});
+
+// If not in production, log to console too
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new transports.Console({
+    format: format.combine(
+      format.colorize(),
+      format.simple()
+    )
+  }));
+}
+
+module.exports = logger;
